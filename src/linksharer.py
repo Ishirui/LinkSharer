1e99772953af3ade77b2766235bc606aa11a5020
Pierre-Louis Veyrenc <veyrencpierrelouis@gmail.com>
2024-03-02 14:04:35 +0100

    feat(basic): Basic share creation and download endpoints

--- src/linksharer.py
+++ src/linksharer.py
@@ -1,11 +1,84 @@
-"""Basic hello-world example app"""
+"""Create links for easy file sharing."""
 
-from flask import Flask
+import logging
+from pathlib import Path
+from typing import Dict
+from uuid import UUID, uuid4
+
+from flask import Flask, Response, abort, request, send_file
 
 app = Flask(__name__)
 
+SHARES: Dict[UUID, Path] = {}
 
-@app.route("/")
-def hello_world():
-    """Default route: returns a 'hello-world' HTML document"""
-    return "<p>Hello, World!</p>"
+
+def create_share(share_path: Path) -> UUID:
+    """
+    Create a new share pointing to this path with a randomly-assigned UUID.
+    Currently only file shares are supported.
+    """
+    if not share_path.exists():
+        raise FileNotFoundError(share_path)
+
+    if not share_path.is_file():
+        raise ValueError(
+            f"Only single-file shares are supported. {share_path!s} is not a file !"
+        )
+
+    share_id = uuid4()
+    SHARES[share_id] = share_path.absolute()
+    logging.info(
+        "Successfully created share '%s', pointing to '%s'",
+        share_id,
+        share_path.absolute(),
+    )
+    return share_id
+
+
+@app.route("/share/<share_id_str>", methods=["GET"])
+def download_endpoint(share_id_str: str) -> Response:
+    """Download the file identified by the share ID"""
+
+    try:
+        share_id = UUID(share_id_str)
+    except ValueError:
+        logging.error("Tried to access invalid UUID '%s' !", share_id_str)
+        abort(400)
+
+    try:
+        share_path = SHARES[share_id]
+    except KeyError:
+        logging.error("Requested UUID '%s' not found !", share_id)
+        abort(404)
+
+    # TODO: SECURITY HOLE ! Need to use send_from_directory and use relative paths instead
+    # This could make Flask send system/config files from the docker container
+    return send_file(share_path, as_attachment=True)
+
+
+@app.route("/new", methods=["POST"])
+def create_share_endpoint():
+    """Create a new share pointing to the path specified in the HTTP form."""
+    try:
+        share_path = request.form["share_path"]
+    except KeyError:
+        logging.error("'share_path' field not present in request.")
+        abort(400)
+
+    path = Path(share_path)
+
+    try:
+        share_id = create_share(path)
+    except (FileNotFoundError, ValueError) as exc:
+        logging.error("Could not create share: %s", exc)
+        abort(400)
+
+    return f"""
+        <h1>Success !</h1>
+        <p>The share was created successfully.</p>
+        <p>Path: {path.absolute()!s}<br>ID: {share_id}</p>
+    """
+
+
+# TODO: Clean this up
+logging.getLogger().setLevel("DEBUG")
